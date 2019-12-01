4aa6c016ddf79754ad6f4c69d3b0577ddb93c595
David Owczarz <owcdaw@gmail.com>
2019-12-01 21:15:46 +0100

    added alphabet letters

--- js/script.js
+++ js/script.js
@@ -6,5 +6,15 @@ const words = [
 	'watermelon'
 ];
 
+const lettersBoard = document.querySelector('.letters');
+
 let word = words[Math.floor(Math.random() * words.length)];
 
+const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+for(let i = 0; i < letters.length; i++) {
+	let letter = document.createElement('div');
+	letter.textContent = letters[i].toUpperCase();
+	letter.classList.add('letters__key');
+	lettersBoard.appendChild(letter);
+}
