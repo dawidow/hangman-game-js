ae16e4eef7c7bfaa419fd25c7b0e399eb9429209
David Owczarz <owcdaw@gmail.com>
2019-12-09 00:47:03 +0100

    almost finished

--- js/script.js
+++ js/script.js
@@ -1,60 +1,108 @@
-const words = [
-	'front end developer',
-	'programming is more than passion',
-	'english is must have language for all developers',
-	'vue is a javascript framework',
-];
-
 const lettersBoard = document.querySelector('.letters'),
 	textArea = document.querySelector('.textArea'),
-	choosenLetters = document.querySelector('.choosenLetters');
+	choosenLetters = document.querySelector('.choosenLetters'),
+	livesCount = document.querySelector('.livesCount'),
+	gameLoss = document.querySelector('.game-loss'),
+	gameWin = document.querySelector('.game-win'),
+	correctPass = document.querySelector('.game__correctPassword'),
+	currentLives = document.querySelector('.game__currentLives'),
+	playAgainButton = [...document.querySelectorAll('.game__button')];
 
-let word = words[Math.floor(Math.random() * words.length)];
-let wordLength = word.length;
-let wordHidden = [];
-let choosenLettersArray = [];
-
-let image = document.querySelector('.hangman-img').src;
-let lives = 4;
 
 const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-const hiddenWord = (word) => {
-	for(let i = 0; i < word.length; i++) {
-		if(word.charAt(i) == ' ') {
-			wordHidden.push('\xa0');
-		} else {
-			wordHidden.push('-');
-		}
-	}
+let image = document.querySelector('.hangman-img'),
+	lettersAggregate = false;
 
-	textArea.textContent = wordHidden.join(' ');
-}
+const startGame = () => {
+	const words = [
+		// 'front end developer',
+		// 'programming is more than passion',
+		// 'english is must have language for all developers',
+		// 'vue is a javascript framework',
+		'ab', 'cde'
+	];
 
-const checkLetter = e => {
-	let eTarget = String(e.target.innerText.toLowerCase());
+	let word = words[Math.floor(Math.random() * words.length)],
+		wordHidden = [],
+		lives = 5,
+		choosenLettersArray = [];
 
-	for(let i = 0; i < wordHidden.length; i++) {
+	image.src = `./assets/hangman_5.svg`;
+	gameLoss.classList.add('hidden');
+	gameWin.classList.add('hidden');
 
-		if(word[i] === eTarget) {
-			wordHidden[i] = eTarget;
-			e.target.classList.add('letters__key--correct', 'letters__key--disabled');
+	const hiddenWord = (word) => {
+		for(let i = 0; i < word.length; i++) {
+			if(word.charAt(i) == ' ') {
+				wordHidden.push('\xa0');
+			} else {
+				wordHidden.push('-');
+			}
 		}
 
 		textArea.textContent = wordHidden.join(' ');
 	}
 
-	choosenLettersArray.push(eTarget);
-	choosenLetters.textContent = [...new Set(choosenLettersArray)];
+	const checkLetter = e => {
+		let eTarget = String(e.target.innerText.toLowerCase()),
+			correctLetter = false;
+
+		for(let i = 0; i < wordHidden.length; i++) {
+
+			if(word[i] === eTarget) {
+				correctLetter = true;
+				wordHidden[i] = eTarget;
+			}
+
+			textArea.textContent = wordHidden.join(' ');
+		}
+
+		if(correctLetter) {
+			for(let j = 0; j < wordHidden.length; j++) {
+				if(word[j] === eTarget) {
+					e.target.classList.add('letters__key--correct', 'letters__key--disabled');
+				}
+			}
+			console.log(lives);
+		} else {
+			image.src = `./assets/hangman_${lives--}.svg`;
+			e.target.classList.add('letters__key--incorrect', 'letters__key--disabled');
+		}
+
+
+		livesCount.textContent = lives;
+		choosenLettersArray.push(eTarget.toUpperCase());
+		choosenLetters.textContent = [...new Set(choosenLettersArray)];
+
+		gameResult(lives, word, wordHidden);
+	}
+
+	if(!lettersAggregate) {
+		lettersAggregate = true;
+		for(let i = 0; i < letters.length; i++) {
+			let letter = document.createElement('div');
+			letter.classList.remove('letters__key--correct', 'letters__key--incorrect', 'letters__key--disabled')
+			letter.textContent = letters[i].toUpperCase();
+			letter.classList.add('letters__key');
+			lettersBoard.appendChild(letter);
+
+			letter.addEventListener('click', checkLetter, false);
+		}
+	}
+
+	hiddenWord(word);
 }
 
-for(let i = 0; i < letters.length; i++) {
-	let letter = document.createElement('div');
-	letter.textContent = letters[i].toUpperCase();
-	letter.classList.add('letters__key');
-	lettersBoard.appendChild(letter);
-
-	letter.addEventListener('click', checkLetter, false);
+const gameResult = (gameChances, word, wordHidden) => {
+	if(word === wordHidden.join('')) {
+		gameWin.classList.remove('hidden');
+		currentLives.textContent = gameChances;
+	} else if(gameChances === 0) {
+		gameLoss.classList.remove('hidden');
+		correctPass.textContent = word;
+	}
 }
 
-window.addEventListener('DOMContentLoaded', hiddenWord(word));
+startGame();
+playAgainButton.forEach(btn => btn.addEventListener('click', startGame, false));
